3d3b10bf0ac87cb301d5a4f23379b551c1b56536
github username <github email address>
2019-07-31 12:46:13 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rack'
 gem 'sinatra'
 gem 'shotgun'
+gem 'pry'
 
 gem 'rspec'
 gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+require 'pry'
+
 class Application < Sinatra::Base
   # Write your code here!
 
+  get '/' do 
+    erb :index
+  end 
+
+  
+  post '/greet' do 
+    
+    erb :greet
+  end 
+  
 end
